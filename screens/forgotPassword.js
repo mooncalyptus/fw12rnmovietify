f59a87f7f0d2ea0687f4b8f401e0c64fb5882259
Rania Zahara <61317162+mooncalyptus@users.noreply.github.com>
2023-01-10 23:27:08 +0700

    added yup validation on register form

--- screens/forgotPassword.js
+++ screens/forgotPassword.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import {Input, FormControl, Stack, Pressable, Button} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import YupPassword from 'yup-password';
+
+const SchemaValidation = Yup.object().shape({
+    email: Yup.string()
+        .email('Please enter valid email')
+        .required('Email address is Required'),
+});
 
 const ForgotPassword = ({navigation}) => {
     return (
@@ -14,29 +23,50 @@ const ForgotPassword = ({navigation}) => {
                 we'll send a link to your email shortly
             </Text>
             <View style={styles.desc}>
-                <FormControl isRequired>
-                    <Stack>
-                        <FormControl.Label>Email</FormControl.Label>
-                        <View style={styles.inputComponent}>
-                            <Input
-                                w={{
-                                    base: '90%',
-                                    md: '25%',
-                                }}
-                                placeholder="Input your Email"
-                            />
-                        </View>
-                        <View style={styles.buttonComponent}>
-                            <Button
-                                style={{width: '90%'}}
-                                onPress={() =>
-                                    navigation.navigate('SetPassword')
-                                }>
-                                Submit
-                            </Button>
-                        </View>
-                    </Stack>
-                </FormControl>
+                <Formik
+                    initialValues={{email: ''}}
+                    validationSchema={SchemaValidation}>
+                    {({
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        errors,
+                        values,
+                    }) => (
+                        <FormControl isRequired>
+                            <Stack>
+                                <FormControl.Label>Email</FormControl.Label>
+                                <View style={styles.inputComponent}>
+                                    <Input
+                                        w={{
+                                            base: '90%',
+                                            md: '25%',
+                                        }}
+                                        onChangeText={handleChange('email')}
+                                        onBlur={handleBlur('email')}
+                                        placeholder="Input your Email"
+                                    />
+                                    {errors.email && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.email}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                </View>
+                                <View style={styles.buttonComponent}>
+                                    <Button
+                                        style={{width: '90%'}}
+                                        onPress={() =>
+                                            navigation.navigate('SetPassword')
+                                        }>
+                                        Submit
+                                    </Button>
+                                </View>
+                            </Stack>
+                        </FormControl>
+                    )}
+                </Formik>
             </View>
         </View>
     );
--- screens/register.js
+++ screens/register.js
@@ -1,152 +1,264 @@
 import React from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  ScrollView,
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import {Input, FormControl, Stack, Pressable, Button} from 'native-base';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import YupPassword from 'yup-password';
+
+YupPassword(Yup);
+const phoneRegExpID = /^(^08)(\d{8,10})$/;
+const SchemaValidation = Yup.object().shape({
+    firstName: Yup.string().required('First Name is required'),
+    lastName: Yup.string().required('Last Name is required'),
+    phoneNumber: Yup.string()
+        .matches(phoneRegExpID, 'Invalid phone number')
+        .required('Required'),
+    email: Yup.string()
+        .email('Please enter valid email')
+        .required('Email address is Required'),
+    password: Yup.string()
+        .password()
+        .required('Enter password')
+        .min(8, 'Min length 8 characters')
+        .minLowercase(1, 'Min lowercase 1')
+        .minUppercase(1, 'Min uppercase 1')
+        .minSymbols(1, 'Min symbol 1')
+        .minNumbers(1, 'Min number 1'),
+});
 
 const Register = ({navigation}) => {
-  const [show, setShow] = React.useState(false);
-  return (
-    <ScrollView style={styles.container}>
-      <View style={styles.content}>
-        <Image
-          source={require('../src/images/logo.png')}
-          style={{width: 100, height: 40}}
-        />
-        <Text style={styles.textFirst}>Sign Up</Text>
-        <Text style={styles.descForms}>Fill your additional details</Text>
-        <View style={styles.descForms}>
-          <FormControl isRequired>
-            <Stack>
-              <View>
-                <FormControl.Label>First Name</FormControl.Label>
-              </View>
-              <View style={styles.inputComponent}>
-                <Input
-                  w={{
-                    base: '90%',
-                    md: '25%',
-                  }}
-                  placeholder="Input your First Name"
+    const [show, setShow] = React.useState(false);
+    return (
+        <ScrollView style={styles.container}>
+            <View style={styles.content}>
+                <Image
+                    source={require('../src/images/logo.png')}
+                    style={{width: 100, height: 40}}
                 />
-              </View>
-              <View style={styles.inputComponent}>
-                <FormControl.Label>Last Name</FormControl.Label>
-              </View>
-              <View style={styles.inputComponent}>
-                <Input
-                  w={{
-                    base: '90%',
-                    md: '25%',
-                  }}
-                  placeholder="Input your Last Name"
-                />
-              </View>
-              <View style={styles.inputComponent}>
-                <FormControl.Label>Phone Number</FormControl.Label>
-              </View>
-              <View style={styles.inputComponent}>
-                <Input
-                  w={{
-                    base: '90%',
-                    md: '25%',
-                  }}
-                  placeholder="Input your Phone Number"
-                />
-              </View>
-              <View style={styles.inputComponent}>
-                <FormControl.Label>Email</FormControl.Label>
-              </View>
-              <View style={styles.inputComponent}>
-                <Input
-                  w={{
-                    base: '90%',
-                    md: '25%',
-                  }}
-                  placeholder="Input your Email"
-                />
-              </View>
-              <View style={styles.inputComponent}>
-                <FormControl.Label>Password</FormControl.Label>
-              </View>
-              <View style={styles.inputComponent}>
-                <Input
-                  w={{
-                    base: '90%',
-                    md: '25%',
-                  }}
-                  type={show ? 'text' : 'password'}
-                  InputRightElement={
-                    <Pressable onPress={() => setShow(!show)}>
-                      <Icon
-                        name={show ? 'visibility' : 'visibility-off'}
-                        size={25}
-                        mr="2"
-                        // style={styles.iconPassword}
-                        color="muted.400"
-                      />
-                    </Pressable>
-                  }
-                  placeholder="Input your Password"
-                />
-              </View>
-              <View style={styles.inputComponent}>
-                <FormControl.HelperText>
-                  Must be atleast 8 characters.
-                </FormControl.HelperText>
-              </View>
-              <View style={styles.inputComponent}>
-                <Button style={{width: '90%'}}>Sign Up</Button>
-              </View>
-              <View style={styles.textSignIn}>
-                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                  <Text>
-                    Already have account ?{' '}
-                    <Text style={styles.signIn}>Sign In</Text>
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </Stack>
-          </FormControl>
-        </View>
-      </View>
-    </ScrollView>
-  );
+                <Text style={styles.textFirst}>Sign Up</Text>
+                <Text style={styles.descForms}>
+                    Fill your additional details
+                </Text>
+                <View style={styles.descForms}>
+                    <Formik
+                        initialValues={{
+                            firstName: '',
+                            lastName: '',
+                            phoneNumber: '',
+                            email: '',
+                            password: '',
+                        }}
+                        validationSchema={SchemaValidation}>
+                        {({
+                            handleChange,
+                            handleBlur,
+                            handleSubmit,
+                            errors,
+                            values,
+                        }) => (
+                            <FormControl isRequired>
+                                <Stack>
+                                    <View>
+                                        <FormControl.Label>
+                                            First Name
+                                        </FormControl.Label>
+                                    </View>
+                                    <View style={styles.inputComponent}>
+                                        <Input
+                                            w={{
+                                                base: '90%',
+                                                md: '25%',
+                                            }}
+                                            onChangeText={handleChange(
+                                                'firstName',
+                                            )}
+                                            onBlur={handleBlur('firstName')}
+                                            placeholder="Input your First Name"
+                                        />
+                                    </View>
+                                    {errors.firstName && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.firstName}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                    <View style={styles.inputComponent}>
+                                        <FormControl.Label>
+                                            Last Name
+                                        </FormControl.Label>
+                                    </View>
+                                    <View style={styles.inputComponent}>
+                                        <Input
+                                            w={{
+                                                base: '90%',
+                                                md: '25%',
+                                            }}
+                                            onChangeText={handleChange(
+                                                'lastName',
+                                            )}
+                                            onBlur={handleBlur('lastName')}
+                                            placeholder="Input your Last Name"
+                                        />
+                                    </View>
+                                    {errors.lastName && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.lastName}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                    <View style={styles.inputComponent}>
+                                        <FormControl.Label>
+                                            Phone Number
+                                        </FormControl.Label>
+                                    </View>
+                                    <View style={styles.inputComponent}>
+                                        <Input
+                                            w={{
+                                                base: '90%',
+                                                md: '25%',
+                                            }}
+                                            onChangeText={handleChange('phoneNumber')}
+                                            onBlur={handleBlur('phoneNumber')}
+                                            placeholder="Input your Phone Number"
+                                        />
+                                    </View>
+                                    {errors.phoneNumber && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.phoneNumber}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                    <View style={styles.inputComponent}>
+                                        <FormControl.Label>
+                                            Email
+                                        </FormControl.Label>
+                                    </View>
+                                    <View style={styles.inputComponent}>
+                                        <Input
+                                            w={{
+                                                base: '90%',
+                                                md: '25%',
+                                            }}
+                                            onChangeText={handleChange('email')}
+                                            onBlur={handleBlur('email')}
+                                            placeholder="Input your Email"
+                                        />
+                                    </View>
+                                    {errors.email && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.email}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                    <View style={styles.inputComponent}>
+                                        <FormControl.Label>
+                                            Password
+                                        </FormControl.Label>
+                                    </View>
+                                    <View style={styles.inputComponent}>
+                                        <Input
+                                            w={{
+                                                base: '90%',
+                                                md: '25%',
+                                            }}
+                                            type={show ? 'text' : 'password'}
+                                            InputRightElement={
+                                                <Pressable
+                                                    onPress={() =>
+                                                        setShow(!show)
+                                                    }>
+                                                    <Icon
+                                                        name={
+                                                            show
+                                                                ? 'visibility'
+                                                                : 'visibility-off'
+                                                        }
+                                                        size={25}
+                                                        mr="2"
+                                                        // style={styles.iconPassword}
+                                                        color="muted.400"
+                                                    />
+                                                </Pressable>
+                                            }
+                                            onChangeText={handleChange('password')}
+                                            onBlur={handleBlur('password')}
+                                            placeholder="Input your Password"
+                                        />
+                                    </View>
+                                    {errors.password && (
+                                        <View style={styles.fourthComponent}>
+                                            <FormControl.HelperText>
+                                                {errors.password}
+                                            </FormControl.HelperText>
+                                        </View>
+                                    )}
+                                    <View style={styles.inputComponent}>
+                                        <Button style={{width: '90%'}}>
+                                            Sign Up
+                                        </Button>
+                                    </View>
+                                    <View style={styles.textSignIn}>
+                                        <TouchableOpacity
+                                            onPress={() =>
+                                                navigation.navigate('Login')
+                                            }>
+                                            <Text>
+                                                Already have account ?{' '}
+                                                <Text style={styles.signIn}>
+                                                    Sign In
+                                                </Text>
+                                            </Text>
+                                        </TouchableOpacity>
+                                    </View>
+                                </Stack>
+                            </FormControl>
+                        )}
+                    </Formik>
+                </View>
+            </View>
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  content: {
-    marginTop: 10,
-    marginLeft: 20,
-  },
-  textFirst: {
-    marginLeft: 7,
-  },
-  descForms: {
-    marginLeft: 7,
-    marginTop: 5,
-  },
-  inputComponent: {
-    marginTop: 5,
-  },
-  textSignIn: {
-    marginHorizontal: 50,
-    marginTop: 15,
-    marginBottom: 20,
-  },
-  signIn: {
-    textDecorationStyle: 'underline',
-    textDecorationStyleColor: 'red',
-  },
+    container: {
+        flex: 1,
+    },
+    content: {
+        marginTop: 10,
+        marginLeft: 20,
+    },
+    textFirst: {
+        marginLeft: 7,
+    },
+    descForms: {
+        marginLeft: 7,
+        marginTop: 5,
+    },
+    inputComponent: {
+        marginTop: 5,
+    },
+    textSignIn: {
+        marginHorizontal: 50,
+        marginTop: 15,
+        marginBottom: 20,
+    },
+    signIn: {
+        textDecorationStyle: 'underline',
+        textDecorationStyleColor: 'red',
+    },
 });
 
 export default Register;
